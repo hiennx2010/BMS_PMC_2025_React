0d3fba32a4c0655eb9a68994764a22e3b7e21cb4
Project <cuongnghiem148@gmail.com>
2025-11-26 08:49:09 +0700

    sửa file .evn

--- Project_Be/src/main-module/entity/thuc-tap/thuc-tap.entity.ts
+++
@@ -1,6 +0,0 @@
-import { Entity, Column, BeforeInsert, BeforeUpdate, BeforeRemove } from 'typeorm';
-import { BaseEntity } from 'src/common-module/entity/base/base.entity';
-
-@Entity({ name: 'tbl_thuc_tap' })
-export class thucTap extends BaseEntity {
-}
--- Project_Be/src/main-module/main.module.ts
+++ Project_Be/src/main-module/main.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from 'src/common-module/common.module';
-import { thucTapController } from './controller/thuc-tap/thuc-tap.controller';
-import { thucTapService } from './service/thuc-tap/thuc-tap.service';
-import { thucTap } from './entity/thuc-tap/thuc-tap.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([thucTap]), CommonModule, MainModule],
-  controllers: [thucTapController],
-  providers: [thucTapService],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([]), CommonModule, MainModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class MainModule {}
