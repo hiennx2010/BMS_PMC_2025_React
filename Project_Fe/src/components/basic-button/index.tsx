5462e5b43ba764bd59914800c723b745dc464929
Project <cuongnghiem148@gmail.com>
2025-12-22 09:47:01 +0700

    sửa cảnh báo button

--- Project_Fe/src/components/basic-button/index.tsx
+++ Project_Fe/src/components/basic-button/index.tsx
@@ -1,23 +1,20 @@
+import { forwardRef } from "react";
 import type { ButtonProps } from "antd";
-import type { ReactNode } from "react";
 import { Button } from "antd";
+import type { ReactNode } from "react";
 
 interface BasicButtonProps extends ButtonProps {
-	children?: ReactNode
+  children?: ReactNode;
 }
 
-export function BasicButton(props: BasicButtonProps) {
-	const { children, ...restProps } = props;
+export const BasicButton = forwardRef<HTMLButtonElement, BasicButtonProps>(
+  function BasicButton(props, ref) {
+    const { children, ...restProps } = props;
 
-	// 清除自定义属性
-	const antdButtonProps: Partial<BasicButtonProps> = { ...restProps };
-
-	return (
-		<Button
-			type="primary"
-			{...antdButtonProps}
-		>
-			{children}
-		</Button>
-	);
-}
+    return (
+      <Button ref={ref} type="primary" {...restProps}>
+        {children}
+      </Button>
+    );
+  }
+);
